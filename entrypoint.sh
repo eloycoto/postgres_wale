2b1ea6ea28c53b9bdba1c04a73a3b560e93e4166
Eloy Coto <eloy.coto@gmail.com>
2017-04-05 14:18:51 +0100

    Fix issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,8 @@ sudo -u postgres $PG_BIN/initdb -D $DB_PATH -A trust
 cp /postgresql.conf $DB_PATH
 cp /pg_hba.conf $DB_PATH
 
-su - postgres -c "envdir /etc/wal-e.d/env /usr/local/bin/wal-e backup-fetch /opt/database LATEST"
+su - postgres -c "envdir /etc/wal-e.d/env /usr/local/bin/wal-e backup-fetch /opt/database LATEST" || exit 1
 sudo -u postgres rm $DB_PATH/backup_label
 sudo -u postgres $PG_BIN/pg_resetxlog -f $DB_PATH
-sudo -u postgres /usr/lib/postgresql/9.6/bin/pg_ctl -D /opt/database/
+echo 1 > /wale_finished
+sudo -u postgres /usr/lib/postgresql/9.6/bin/postgres -D /opt/database/
